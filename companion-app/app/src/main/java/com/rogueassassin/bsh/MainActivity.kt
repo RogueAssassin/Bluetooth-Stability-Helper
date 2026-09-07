f83a6388556279f2e0f52fa30d801721c4c53ec3
Stephen Feil <rogue@roguegaming.com.au>
2026-09-08 06:37:12 +1000

    Fix diagnostic coroutine build

--- companion-app/app/src/main/java/com/rogueassassin/bsh/MainActivity.kt
+++ companion-app/app/src/main/java/com/rogueassassin/bsh/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 private val Bg = Color(0xFF090B10)
